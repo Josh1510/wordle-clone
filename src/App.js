5345accd08d111cf337869e5471dc05642174b9b
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-07 16:04:16 +1100

    removed duplicated body tag

--- src/App.js
+++ src/App.js
@@ -5,96 +5,95 @@ function App() {
   return (
     <div>
       <header></header>
-      <body>
-        <div className="keyboard">
-          <button className="key" data-key="Q">
-            Q
-          </button>
-          <button className="key" data-key="W">
-            W
-          </button>
-          <button className="key" data-key="E">
-            E
-          </button>
-          <button className="key" data-key="R">
-            R
-          </button>
-          <button className="key" data-key="T">
-            T
-          </button>
-          <button className="key" data-key="Y">
-            Y
-          </button>
-          <button className="key" data-key="U">
-            U
-          </button>
-          <button className="key" data-key="I">
-            I
-          </button>
-          <button className="key" data-key="O">
-            O
-          </button>
-          <button className="key" data-key="P">
-            P
-          </button>
-          <div className="buttonSpace"></div>
-          <button className="key" data-key="A">
-            A
-          </button>
-          <button className="key" data-key="S">
-            S
-          </button>
-          <button className="key" data-key="D">
-            D
-          </button>
-          <button className="key" data-key="F">
-            F
-          </button>
-          <button className="key" data-key="G">
-            G
-          </button>
-          <button className="key" data-key="H">
-            H
-          </button>
-          <button className="key" data-key="J">
-            J
-          </button>
-          <button className="key" data-key="K">
-            K
-          </button>
-          <button className="key" data-key="L">
-            L
-          </button>
-          <div className="buttonSpace"></div>
-          <button className="key large" data-enter>
-            ENTER
-          </button>
-          <button className="key" data-key="Z">
-            Z
-          </button>
-          <button className="key" data-key="X">
-            X
-          </button>
-          <button className="key" data-key="C">
-            C
-          </button>
-          <button className="key" data-key="V">
-            V
-          </button>
-          <button className="key" data-key="B">
-            B
-          </button>
-          <button className="key" data-key="N">
-            N
-          </button>
-          <button className="key" data-key="M">
-            M
-          </button>
-          <button className="key large" data-backspace>
-            <BackspaceImg />
-          </button>
-        </div>
-      </body>
+
+      <div className="keyboard">
+        <button className="key" data-key="Q">
+          Q
+        </button>
+        <button className="key" data-key="W">
+          W
+        </button>
+        <button className="key" data-key="E">
+          E
+        </button>
+        <button className="key" data-key="R">
+          R
+        </button>
+        <button className="key" data-key="T">
+          T
+        </button>
+        <button className="key" data-key="Y">
+          Y
+        </button>
+        <button className="key" data-key="U">
+          U
+        </button>
+        <button className="key" data-key="I">
+          I
+        </button>
+        <button className="key" data-key="O">
+          O
+        </button>
+        <button className="key" data-key="P">
+          P
+        </button>
+        <div className="buttonSpace"></div>
+        <button className="key" data-key="A">
+          A
+        </button>
+        <button className="key" data-key="S">
+          S
+        </button>
+        <button className="key" data-key="D">
+          D
+        </button>
+        <button className="key" data-key="F">
+          F
+        </button>
+        <button className="key" data-key="G">
+          G
+        </button>
+        <button className="key" data-key="H">
+          H
+        </button>
+        <button className="key" data-key="J">
+          J
+        </button>
+        <button className="key" data-key="K">
+          K
+        </button>
+        <button className="key" data-key="L">
+          L
+        </button>
+        <div className="buttonSpace"></div>
+        <button className="key large" data-enter>
+          ENTER
+        </button>
+        <button className="key" data-key="Z">
+          Z
+        </button>
+        <button className="key" data-key="X">
+          X
+        </button>
+        <button className="key" data-key="C">
+          C
+        </button>
+        <button className="key" data-key="V">
+          V
+        </button>
+        <button className="key correct" data-key="B">
+          B
+        </button>
+        <button className="key wrongPosition" data-key="N">
+          N
+        </button>
+        <button className="key incorrect" data-key="M">
+          M
+        </button>
+        <button className="key large" data-backspace>
+          <BackspaceImg />
+        </button>
+      </div>
     </div>
   );
 }
