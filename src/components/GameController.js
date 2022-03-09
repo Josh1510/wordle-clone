a1252d32b59c0dbee7c09777d5f458b97d034996
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-09 15:14:42 +1100

    functions to detect user clicking or pressing keyboard

--- src/components/GameController.js
+++ src/components/GameController.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AnswerGrid from './AnswerGrid';
 import Keyboard from './Keyboard';
 
 const GameController = () => {
+  // Handles the user clicking on the keyboard with their mouse
+  const handleClick = (event) => {
+    if (event.target.matches('[data-key]')) {
+      console.log(event.target.dataset.key);
+      return;
+    }
+    if (event.target.matches('[data-backspace]')) {
+      console.log(event.target.dataset.backspace);
+      return;
+    }
+    if (event.target.matches('[data-enter]')) {
+      console.log(event.target.dataset.enter);
+      return;
+    }
+  };
+
+  // Handles keyboard input by the user
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      //regex to fire function only if a letter key is pressed
+      if (event.key.match(/[a-z]/)) {
+        console.log(event.key);
+        return;
+      }
+      if (event.key === 'Backspace' || event.key === 'Delete') {
+        console.log(event.key);
+        return;
+      }
+      if (event.key === 'Enter') {
+        console.log(event.key);
+        return;
+      }
+      console.log(event);
+    };
+
+    document.addEventListener('keydown', handleKeyPress);
+
+    return function cleanup() {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
   return (
     <div>
       <AnswerGrid />
-      <Keyboard />
+      <Keyboard handleClick={handleClick} />
     </div>
   );
 };
