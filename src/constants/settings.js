ca4ecdc64da6245a2b90be6e98be57c21eb87a1d
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-10 17:32:20 +1100

    added const for start date and todays answer

--- src/constants/settings.js
+++ src/constants/settings.js
@@ -1,2 +1,6 @@
+import { ANSWER_LIST } from './answerList';
+
 export const MAX_WORD_LENGTH = 5;
 export const MAX_ATTEMPTS = 6;
+export const START_DATE = new Date(2021, 5, 19);
+export const TODAY_ANSWER = ANSWER_LIST[Math.floor((new Date() - START_DATE) / 1000 / 60 / 60 / 24)];
