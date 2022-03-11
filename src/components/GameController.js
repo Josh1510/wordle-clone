0e9eeb44ecbd6d69a0d30a52d3de1a3b641a0e00
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-11 17:08:11 +1100

    moving keyboard functions to keyboard component

--- src/components/GameController.js
+++ src/components/GameController.js
@@ -11,55 +11,14 @@ const GameController = () => {
   const [gameOver, setGameOver] = useState(false);
 
   const [guesses, setGuesses] = useState(['wahht', 'aaavz', 'aahht']);
-  const [currentGuess, setCurrentGuess] = useState('hem  ');
+  const [currentGuess, setCurrentGuess] = useState(['h', 'e', 'm']);
 
   console.log(TODAY_ANSWER);
-
-  // Handles the user clicking on the keyboard with their mouse
-  const handleClick = (event) => {
-    if (event.target.matches('[data-key]')) {
-      console.log(event.target.dataset.key);
-      return;
-    }
-    if (event.target.matches('[data-backspace]')) {
-      console.log(event.target.dataset.backspace);
-      return;
-    }
-    if (event.target.matches('[data-enter]')) {
-      console.log(event.target.dataset.enter);
-      return;
-    }
-  };
-
-  // Handles keyboard input by the user
-  useEffect(() => {
-    const handleKeyPress = (event) => {
-      //regex to fire function only if a letter key is pressed
-      if (event.key.match(/(\b[a-z{1}]\b)/)) {
-        console.log(`letter key: ${event.key}`);
-        return;
-      }
-      if (event.key === 'Backspace' || event.key === 'Delete') {
-        console.log(`backspace: ${event.key}`);
-        return;
-      }
-      if (event.key === 'Enter') {
-        console.log(`enter: ${event.key}`);
-        return;
-      }
-    };
-
-    document.addEventListener('keydown', handleKeyPress);
-
-    return function cleanup() {
-      document.removeEventListener('keydown', handleKeyPress);
-    };
-  }, []);
 
   return (
     <div>
       <AnswerGrid guesses={guesses} currentGuess={currentGuess} />
-      <Keyboard handleClick={handleClick} />
+      <Keyboard currentGuess={currentGuess} setCurrentGuess={setCurrentGuess} />
     </div>
   );
 };
