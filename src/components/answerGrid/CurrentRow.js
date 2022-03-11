3ed60f8b0204573eaf35b76cbf863dfb54f5978b
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-11 17:08:34 +1100

    displaying empty tiles if guess is not 5 letters long

--- src/components/answerGrid/CurrentRow.js
+++ src/components/answerGrid/CurrentRow.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { MAX_WORD_LENGTH } from '../../constants/settings';
 
 export default function CurrentRow({ currentGuess }) {
-  let rowGuess = Array.from(currentGuess);
+  // create an array to fill out the empty cells
+  const emptyTiles = Array.from({ length: MAX_WORD_LENGTH - currentGuess.length }, (v, i) => i);
 
   return (
     <div className="answer-grid__row">
-      {rowGuess.map((letter, i) => (
-        <div className="answer-grid__tile" data-state={letter === ' ' ? 'empty' : 'notConfirmed'} key={i}>
+      {currentGuess.map((letter, i) => (
+        <div className="answer-grid__tile" data-state="notConfirmed" key={i}>
           {letter}
         </div>
+      ))}
+      {emptyTiles.map((i) => (
+        <div className="answer-grid__tile" data-state="empty" key={i}></div>
       ))}
     </div>
   );
