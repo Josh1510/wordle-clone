2ceae133e5f7dc0b51f864c6594ac3b1d51235e6
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-15 21:28:09 +1100

    change to how animating is implimented

--- src/components/answerGrid/CurrentRow.js
+++ src/components/answerGrid/CurrentRow.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { MAX_WORD_LENGTH } from '../../constants/settings';
 
-export default function CurrentRow({ currentGuess, notEnoughLetters }) {
+export default function CurrentRow({ currentGuess, isAnimating }) {
   // create an array to fill out the empty cells
   const roundGuess = Array.from(currentGuess);
   const emptyTiles = Array.from({ length: MAX_WORD_LENGTH - roundGuess.length }, (v, i) => i);
 
   return (
-    <div className={`answer-grid__row ${notEnoughLetters ? 'invalid' : ''}`}>
+    <div className={`answer-grid__row ${isAnimating ? 'invalid' : ''}`}>
       {roundGuess.map((letter, i) => (
         <div className="answer-grid__tile" data-state="notConfirmed" key={i}>
           {letter}
