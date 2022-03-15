7c5231fb86e411db9ca6a4c44dbc8e841b923497
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-15 21:27:52 +1100

    change to props

--- src/components/answerGrid/AnswerGrid.js
+++ src/components/answerGrid/AnswerGrid.js
@@ -5,7 +5,7 @@ import GuessedRow from './GuessedRow';
 import CurrentRow from './CurrentRow';
 import EmptyRow from './EmptyRow';
 
-const AnswerGrid = ({ guesses, currentGuess, notEnoughLetters, setAlertVisible, setErrorMessage }) => {
+const AnswerGrid = ({ guesses, currentGuess, isAnimating }) => {
   let emptyRows = Array.from({ length: MAX_ATTEMPTS - guesses.length - 1 }, (v, i) => i);
 
   return (
@@ -14,12 +14,7 @@ const AnswerGrid = ({ guesses, currentGuess, notEnoughLetters, setAlertVisible, 
         <GuessedRow key={i} guess={guess} />
       ))}
 
-      <CurrentRow
-        currentGuess={currentGuess}
-        notEnoughLetters={notEnoughLetters}
-        setAlertVisible={setAlertVisible}
-        setErrorMessage={setErrorMessage}
-      />
+      <CurrentRow currentGuess={currentGuess} isAnimating={isAnimating} />
 
       {emptyRows.map((_, i) => (
         <EmptyRow key={i} />
