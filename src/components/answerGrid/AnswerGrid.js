2c282b255a5c393e123b46de9d851197aae80fe8
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-15 17:50:05 +1100

    passing states

--- src/components/answerGrid/AnswerGrid.js
+++ src/components/answerGrid/AnswerGrid.js
@@ -5,7 +5,7 @@ import GuessedRow from './GuessedRow';
 import CurrentRow from './CurrentRow';
 import EmptyRow from './EmptyRow';
 
-const AnswerGrid = ({ guesses, currentGuess }) => {
+const AnswerGrid = ({ guesses, currentGuess, notEnoughLetters, setAlertVisible, setErrorMessage }) => {
   let emptyRows = Array.from({ length: MAX_ATTEMPTS - guesses.length - 1 }, (v, i) => i);
 
   return (
@@ -14,7 +14,12 @@ const AnswerGrid = ({ guesses, currentGuess }) => {
         <GuessedRow key={i} guess={guess} />
       ))}
 
-      <CurrentRow currentGuess={currentGuess} />
+      <CurrentRow
+        currentGuess={currentGuess}
+        notEnoughLetters={notEnoughLetters}
+        setAlertVisible={setAlertVisible}
+        setErrorMessage={setErrorMessage}
+      />
 
       {emptyRows.map((_, i) => (
         <EmptyRow key={i} />
