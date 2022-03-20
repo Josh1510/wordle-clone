99c56f6f57d5aed727882b6940e23640acfc96c6
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-20 18:39:28 +1100

    change to how empty row displayed

--- src/components/answerGrid/EmptyRow.js
+++ src/components/answerGrid/EmptyRow.js
@@ -3,11 +3,11 @@ import { MAX_WORD_LENGTH } from '../../constants/settings';
 import React from 'react';
 
 export default function EmptyRow() {
-  let emptyRow = Array.from({ length: MAX_WORD_LENGTH }, (v, i) => i);
+  const emptyTiles = Array.from(Array(MAX_WORD_LENGTH));
 
   return (
     <div className="answer-grid__row">
-      {emptyRow.map((_, i) => (
+      {emptyTiles.map((_, i) => (
         <div className="answer-grid__tile" data-state="empty" key={i}></div>
       ))}
     </div>
