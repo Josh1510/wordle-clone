6b68e063c5e2b2c019f76d8b32d41098bdd4e154
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-21 14:51:02 +1100

    move file location

--- src/components/alert/Alert.js
+++ src/components/alert/Alert.js
@@ -4,6 +4,7 @@ import './Alert.css';
 export default function Alert({ alertMessage, isGameActive }) {
   return (
     <div className="alert-container">
+      {/* keep alert on screen if game is over. */}
       <div className={isGameActive ? 'alert__message-fade' : 'alert__message-remain'}>{alertMessage}</div>
     </div>
   );
