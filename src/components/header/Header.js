8adeffcd723fc66fb6d0db94c7b487391b09ac66
Joshua Moon <joshmoon.1510@gmail.com>
2022-03-21 14:28:16 +1100

    header content

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,5 +2,17 @@ import React from 'react';
 import './Header.css';
 
 export default function Header() {
-  return <div className="header-container">Wordle Clone</div>;
+  return (
+    <div className="header__container">
+      <div className="header__title">Wordle Clone</div>
+      <div className="gitHub-link__container">
+        <a href="https://github.com/Josh1510/wordle-clone">
+          <div className="gitHub-link__content">
+            <div>GitHub Link</div>
+            <img src="GitHub-Mark-Light-32px.png" alt="GitHub Link" />
+          </div>
+        </a>
+      </div>
+    </div>
+  );
 }
